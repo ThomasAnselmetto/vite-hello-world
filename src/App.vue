7348e35aa1796211b71c1d6ebb10595dcebc85e7
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-27 14:20:09 +0100

    vite init commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>La Mia Prima App Con Vite!</h1>
+</template>
+
+<style></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <section></section>
+</template>
+
+<style scoped>
+section {
+  background-image: url(https://a.espncdn.com/photo/2020/0912/r744504_1296x729_16-9.jpg);
+  background-size: cover;
+  background-position: center;
+}
+</style>
